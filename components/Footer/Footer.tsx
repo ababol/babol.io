f0ad866e3ed4b5c25b970eda73115336ce5079ba
Arnaud Babol <a@babol.io>
2024-05-08 17:36:12 +0200

    Do not use link

--- components/Footer/Footer.tsx
+++ components/Footer/Footer.tsx
@@ -6,20 +6,20 @@ import { SocialLinks } from './SocialLinks';
 
 export const Footer = () => (
   <footer className="flex items-center justify-between pb-16 pt-32">
-    <a
+    <div
       className={cn(
         'cursor-pointer text-3xl font-medium text-black',
         'transition-transform duration-100 ease-in-out',
-        'hover:text-primary hover:-translate-y-1',
-        'focus:text-primary focus:-translate-y-1'
+        'hover:-translate-y-1 hover:text-primary',
+        'focus:-translate-y-1 focus:text-primary'
       )}
       onClick={() => window.scrollTo({ top: 0, behavior: 'smooth' })}
     >
       Up
-    </a>
+    </div>
     <div className="ml-auto flex items-center">
       <Link
-        className="hover:text-primary focus:text-primary mr-5 font-extralight text-black"
+        className="mr-5 font-extralight text-black hover:text-primary focus:text-primary"
         href="mailto:a@babol.io"
       >
         a@babol.io
